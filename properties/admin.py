ff2eb30d1a6070228c8a5130abc6c65037bc9a5f
cloudrohu <cloudrohu@gmail.com>
2023-12-11 16:00:53 +0530

    title

--- properties/admin.py
+++ properties/admin.py
@@ -69,21 +69,24 @@ class LocalityAdmin(DraggableMPTTAdmin):
 @admin_thumbnails.thumbnail('image')
 class DeveloperAdmin(admin.ModelAdmin):
     list_display = ['id','title', 'contact_person','contact_no','email','address','locality','city','image_thumbnail']
-
+    list_display = ['id','title','locality','city','propert_type', 'image_thumbnail']
+    list_editable=('title','locality','city',) 
+    list_filter = ('locality','city',) 
+    search_fields = ('title')
 
 @admin_thumbnails.thumbnail('image')
 class Residential_ProjectAdmin(admin.ModelAdmin):
     list_display = ['id','title','locality','city','propert_type', 'image_thumbnail']
     list_editable=('title','locality','city',) 
     list_filter = ('locality','city',) 
-    search_fields = ('title','locality','city',)
+    search_fields = ('title')
 
 @admin_thumbnails.thumbnail('image')
 class Commercial_ProjectAdmin(admin.ModelAdmin):
     list_display = ['id','title','locality','city','propert_type','image_thumbnail']
     list_editable=('title','locality','city',) 
     list_filter = ('locality','city',) 
-    search_fields = ('title','locality','city',)
+    search_fields = ('title')
 
 
 
